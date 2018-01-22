2b1db015bf38a296e0af39fd9da1b867c737ea72
chamod <chamod.14@cse.mrt.ac.lk>
2018-01-22 15:24:36 +0530

    Add hello world program with multiple threads

---
+++ cmake-build-debug/CMakeFiles/concurrent_linked_list.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/concurrent_linked_list.dir/main.c.o"
+  "concurrent_linked_list.pdb"
+  "concurrent_linked_list"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/concurrent_linked_list.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
